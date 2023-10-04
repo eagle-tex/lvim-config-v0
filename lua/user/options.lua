4f8750eef8776730a8a3772450085f395dc91078
Gildas Montcho <gmontcho@gmail.com>
2023-10-05 00:40:43 +0200

    feat(options.lua): allow neovim to access the system clipboard
    - requires a clipboard provider (such as xclip for example)
        `sudo apt install xclip`
    
    - run `:h clipboard` for help

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -7,3 +7,5 @@ vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2 -- insert 2 spaces for a tab
 vim.opt.wrap = true -- wrap lines
+
+vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
