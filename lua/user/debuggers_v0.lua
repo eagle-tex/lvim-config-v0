223e10339d1a04be7b2ca354afa11e033408577d
Gildas Montcho <gmontcho@gmail.com>
2023-10-12 22:35:48 +0200

    docs(debuggers_v0.lua): temporary save

--- lua/user/debuggers_v0.lua
+++ lua/user/debuggers_v0.lua
@@ -2,10 +2,10 @@ local dap = require("dap")
 -- Set keymaps to control the debugger
 -- https://miguelcrespo.co/posts/debugging-javascript-applications-with-neovim/
 vim.keymap.set("n", "<F5>", require("dap").continue)
+vim.keymap.set("n", "<F9>", require("dap").toggle_breakpoint)
 vim.keymap.set("n", "<F10>", require("dap").step_over)
 vim.keymap.set("n", "<F11>", require("dap").step_into)
 vim.keymap.set("n", "<F12>", require("dap").step_out)
-vim.keymap.set("n", "<leader>b", require("dap").toggle_breakpoint)
 vim.keymap.set("n", "<leader>B", function()
 	require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
 end)
