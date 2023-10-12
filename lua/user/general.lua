a1de71bed86810327a616dfe8eb1405e23a83a3f
Gildas Montcho <gmontcho@gmail.com>
2023-10-12 22:29:14 +0200

    feat: add tailwind config (not working yet)

--- lua/user/general.lua
+++ lua/user/general.lua
@@ -2,5 +2,12 @@
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true -- false by default
 lvim.colorscheme = "lunar"
+
+local opts = {}
+require("lvim.lsp.manager").setup("tailwindcss", opts)
+
+-- lvim.builtin.cmp.formatting = {
+-- 	format = require("tailwindcss-colorizer-cmp").formatter,
+-- }
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
